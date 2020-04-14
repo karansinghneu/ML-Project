08041db4ece9e02440e0260a44ba28debcb3c314
karan Singh <singh.kara@husky.neu.edu>
2020-04-13 20:14:39 -0400

    Works with the latest versions, infersent methods left to be figured out

--- src/create_embeddings.py
+++ src/create_embeddings.py
@@ -11,29 +11,15 @@ import spacy
 
 en_nlp = spacy.load('en')
 
-if __name__ == '__main__':
 
-    ## Convert Json to Pandas Dataframe
-
-    train = pd.read_json("data/train-v1.1.json")
-
-    valid = pd.read_json("data/dev-v1.1.json")
-
-    print("Training set: ", train.shape)
-    train.head(3)
-    print(train.iloc[1, 0]['paragraphs'][0])
-
-    print("Validation set: ", valid.shape)
-    valid.head(3)
-    # valid.iloc[1,0]['paragraphs'][0]
-
+def populate_dataframe(training):
     contexts = []
     questions = []
     answers_text = []
     answers_start = []
 
-    for i in range(train.shape[0]):
-        topic = train.iloc[i, 0]['paragraphs']
+    for i in range(training.shape[0]):
+        topic = training.iloc[i, 0]['paragraphs']
         for sub_para in topic:
             for q_a in sub_para['qas']:
                 questions.append(q_a['question'])
@@ -41,10 +27,14 @@ if __name__ == '__main__':
                 answers_text.append(q_a['answers'][0]['text'])
                 contexts.append(sub_para['context'])
     df = pd.DataFrame({"context": contexts, "question": questions, "answer_start": answers_start, "text": answers_text})
+    df.to_csv("data/train.csv", index=False)
+    return df
 
-    df.to_csv("data/train.csv", index=None)
 
-    paras = list(df["context"].drop_duplicates().reset_index(drop=True))
+def generate_embeddings(training):
+    dataframe = populate_dataframe(training)
+
+    paras = list(dataframe["context"].drop_duplicates().reset_index(drop=True))
 
     blob = TextBlob(" ".join(paras))
     sentences = [item.raw for item in blob.sentences]
@@ -68,7 +58,7 @@ if __name__ == '__main__':
             print(i)
         dict_embeddings[sentences[i]] = infersent.encode([sentences[i]], tokenize=True)
 
-    questions = list(df["question"])
+    questions = list(dataframe["question"])
 
     print("Building questions dict")
     # for i in range(len(questions)):
@@ -80,12 +70,30 @@ if __name__ == '__main__':
     d1 = {key: dict_embeddings[key] for i, key in enumerate(dict_embeddings) if i % 2 == 0}
     d2 = {key: dict_embeddings[key] for i, key in enumerate(dict_embeddings) if i % 2 == 1}
 
-    with open('data/dict_embeddings1.pickle', 'wb') as handle:
+    with open('data/full_data/dict_embeddings1.pickle', 'wb') as handle:
         pickle.dump(d1, handle)
-        print("Created 'data/dict_embeddings1.pickl")
+        print("Created 'data/full_data/dict_embeddings1.pickl")
 
-    with open('data/dict_embeddings2.pickle', 'wb') as handle:
+    with open('data/full_data/dict_embeddings2.pickle', 'wb') as handle:
         pickle.dump(d2, handle)
-        print("Created 'data/dict_embeddings2.pickl")
+        print("Created 'data/full_data/dict_embeddings2.pickl")
+    return dict_embeddings, d1, d2
 
-    del dict_embeddings
+
+if __name__ == '__main__':
+
+    train = pd.read_json("data/train-v1.1.json")
+
+    valid = pd.read_json("data/dev-v1.1.json")
+
+    print("Training set: ", train.shape)
+    print('Train Head', train.head(3))
+    print('The training i-loc part', train.iloc[1, 0]['paragraphs'][0])
+
+    print("Validation set: ", valid.shape)
+    print('Validation head', valid.head(3))
+    print('The validation i-loc part', valid.iloc[1, 0]['paragraphs'][0])
+    final_embeddings, embedding_first, embedding_second = generate_embeddings(train)
+    print('Full Dict Embeddings', final_embeddings)
+    print('The dictionary 1 is:', embedding_first)
+    print('The dictionary 2 is:', embedding_second)
